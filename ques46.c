c747726eba678e595265ba6b81d2448218afc4a2
Satyam Rastogi <satyamrastogi2007@gmail.com>
2025-09-12 19:09:12 +0530

    Add files via upload

---
+++ ques46.c
@@ -0,0 +1,22 @@
+/*Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+
+#include<stdio.h>
+int main () {
+    int i,j,n;
+    printf("enter the number of rows: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=n;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
