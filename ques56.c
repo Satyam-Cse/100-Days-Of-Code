d5d6cf0d0517b5b5589416909127fbf781d24455
Satyam Rastogi <satyamrastogi2007@gmail.com>
2025-09-17 23:28:56 +0530

    Add files via upload

---
+++ ques56.c
@@ -0,0 +1,10 @@
+//Read and print elements of a one-dimensional array.
+
+#include<stdio.h>
+int main () {
+    int arr[5]= {5,10,15,20,25,};
+for(int i=1;i<5;i++) {
+    printf("%d \n",arr[i]);
+}
+return 0;
+}
