75c58528b8af72efcbf37bcae66655b7277539cb
Satyam Rastogi <satyamrastogi2007@gmail.com>
2025-11-28 23:03:08 +0530

    Add files via upload

---
+++ ques135.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+enum Result {
+    a = 10,
+    b = 20,
+    c = 30
+};
+
+int main() {
+
+    printf( "  a  = %d\n", a);
+    printf( "  b  = %d\n", b);
+    printf( " c  = %d\n", c);
+
+    return 0;
+}
